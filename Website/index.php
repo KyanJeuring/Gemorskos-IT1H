b8230fa93798d7e577d01c4f1bd8433dffcbe9e1
KyanJeuring <kyanjeuring@gmail.com>
2024-12-03 16:36:17 +0100

    Add initial HTML structure and CSS styles for the website

---
+++ Website/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Gemorskos News Agency</title>
+        <link rel="stylesheet" href="./CSS/main.css" type="text/css">
+        <link rel="Icon" href="./Assets/favicon/favicon.png" type="image/x-icon">
+    </head>
+    <body>
+        <p>Hello World!</p>
+    </body>
+</html>
