3c3d82645ad10a57f4ba3c3242287da2b162ed9e
KyanJeuring <kyanjeuring@gmail.com>
2024-12-15 14:24:56 +0100

    Update favicon from PNG to ICO format

--- Website/index.php
+++ Website/index.php
@@ -5,7 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Gemorskos News Agency</title>
         <link rel="stylesheet" href="./CSS/main.css" type="text/css">
-        <link rel="icon" href="./Assets/favicon/favicon.png" type="image/x-icon">
+        <link rel="icon" href="./Assets/favicon/favicon.ico" type="image/x-icon">
     </head>
     <body>
         <main>
